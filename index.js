b230a32cf7c45b496a2da124848fdbffd3786756
Brannen Petit <bpetit940@gmail.com>
2020-03-20 15:15:00 -0500

    working on adding module

---
+++ index.js
@@ -0,0 +1,53 @@
+import store from "./store";
+
+function initialLoad() {
+  let initial = document.createElement("section");
+  let initialItems = store.store.map(
+    item => ` <section class="item">
+          <form>
+            <label for="itemUpdater"></label>
+            <input
+              type="text"
+              name="itemUpdater"
+              class='itemUdapter'
+              value=${item.name}
+            ></input>
+          </form>
+          <button>done</button>
+          <button>delete</button>
+        </section>`
+  );
+  initial.setAttribute = ("id", "initial-items");
+  initial.innerHTML = initialItems;
+  document.body.appendChild(initial);
+}
+
+function addItem() {
+  let newHtmlEl = document.createElement("section");
+  let input = document.getElementById("Todoinput").value;
+  newHtmlEl.setAttribute = ("id", "dome");
+  newHtmlEl.innerHTML = ` <section class="item">
+          <form>
+            <label for="itemUpdater"></label>
+            <input
+              type="text"
+              name="itemUpdater"
+              class='itemUdapter'
+              value=${input}
+            ></input>
+          </form>
+          <button>done</button>
+          <button>delete</button>
+        </section>`;
+  document.body.appendChild(newHtmlEl);
+  //   let input = document.getElementById("Todoinput").value;
+  //   console.log(`${input}`);
+  //   let newHtml = `
+  //     <section class="item">
+  //         Hello
+  //     </section>`;
+  //   document.getElementById("dome").innerHtml = "hi";
+}
+
+initialLoad();
+document.getElementById("todoBtn").addEventListener("click", () => addItem());
---
+++ store.js
@@ -0,0 +1,6 @@
+const store = [
+  { name: "item1", checked: true },
+  { name: "item2", checked: false }
+];
+
+export default store;
